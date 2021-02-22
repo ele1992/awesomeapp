e82c2ce7f5c618540e43fd1bb00b79bb0408faa2
ele1992 <tjin.yip@gmail.com>
2021-02-22 14:31:49 +0100

    added title component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from ".components/Title.js";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title />
+      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
+        <p>Hello!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
