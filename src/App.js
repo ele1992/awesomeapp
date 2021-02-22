60e1047aaff293c1bac1b1a83a0461244fbb4b63
ele1992 <tjin.yip@gmail.com>
2021-02-22 11:57:45 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react 
+          Welcome to Codaisseur
         </a>
       </header>
     </div>
